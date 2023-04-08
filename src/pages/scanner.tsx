c6bd404f62f6686bacee6ec2adf4e1ba1d1bca30
Nicolas Liu <nicolasliu18@gmail.com>
2023-04-08 12:00:11 -0400

    commmit

--- src/pages/scanner.tsx
+++ src/pages/scanner.tsx
@@ -1,12 +1,9 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 
-import { api } from "~/utils/api";
+// import { api } from "~/utils/api";
 
 const Scanner: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
