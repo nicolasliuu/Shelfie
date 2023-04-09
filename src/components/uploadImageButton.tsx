01d826e6cba9fd4786a7ee8d024d55ffbaf4ba46
Nicolas Liu <nicolasliu18@gmail.com>
2023-04-09 05:11:55 -0400

    style

--- src/components/uploadImageButton.tsx
+++ src/components/uploadImageButton.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable */
 import React, { useState } from 'react';
-import { HfInference } from '@huggingface/inference';
+import { HfInference, ObjectDetectionReturnValue } from '@huggingface/inference';
 import { AnyAttrs } from '@tensorflow/tfjs';
 
 const UploadImageButton = () => {
   const [image, setImage] = useState(null);
   const [label, setLabel] = useState<string>('');
 
-  const handleUpload = async (event: any) => {
   const handleUpload = async (event: any) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
     const file = event.target.files[0];
@@ -21,9 +20,10 @@ const UploadImageButton = () => {
         model: 'facebook/detr-resnet-50',
       });
       console.log(response);
-    //   setLabel(response.label);
+      response.forEach((obj: ObjectDetectionReturnValue) => {
+        console.log(obj.label);
+      });
       console.log(label);
-    }
     };
 
     reader.readAsDataURL(file);
@@ -31,7 +31,7 @@ const UploadImageButton = () => {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
-      <label htmlFor="image-upload" style={{ backgroundColor: '#0078D4', color: 'white', padding: '10px 20px', borderRadius: '5px', cursor: 'pointer' }}>Or Upload An Image</label>
+      <label htmlFor="image-upload" style={{ backgroundColor: '#3767BF', color: 'white', padding: '10px 20px', borderRadius: '5px', cursor: 'pointer' }}>Or Upload An Image</label>
       <input id="image-upload" type="file" accept="image/*" onChange={handleUpload} style={{ display: 'none' }} />
     </div>
   );
